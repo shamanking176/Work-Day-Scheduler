b6c37d4241190aee32971ea2b6b1b86080bae2c1
Slug <adamloppe6@gmail.com>
2023-02-28 21:54:41 -0500

    Fixed classes

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 var saveBtn=$('.saveBtn');
 var timeblock=$('.time-block');
 var currentDay= dayjs().format('dddd, MMMM Do');
@@ -10,40 +8,45 @@ $('#currentDay').text(currentDay);
 
 
 $(function () {
+  var currentHour = dayjs().get('hour');
+
     
     saveBtn.on('click',function(){
-      var textarea= $(this).siblings("textarea").val();
+      var textarea= $(this).siblings('.description').val();
       var hourid=$(this).parent().attr('id');
       localStorage.setItem(hourid, textarea);
-    })
-
-      var currentHour = dayjs().get('hour');
 
 
-      timeblock.each(function (){
-        var hour = $(this).attr('id');
-        var hourText = parseInt(hour);
+     
+    });
 
-        if(currentHour=== hourText){
-          $(this).addClass('present');
-        }
-        else if(currentHour < hourText){
-          $(this).addClass('future');
-        }
+    timeblock.each(function (){
+      var hour = $(this).attr('id');
+      var hourText = parseInt(hour);
 
-        else {
-          $(this).addClass('past');
-        }
+      if(currentHour === hourText){
+        $(this).addClass('present'); 
+      }
+      else if(currentHour < hourText){
+        $(this).addClass('future');
+      }
+
+      else {
+        $(this).addClass('past');
+      }
 
 
 
-      });
+    });
+      
+
+      
 
       timeblock.each(function(){
         var id= $(this).attr('id');
         var storageValue = localStorage.getItem(id);
         var newValue = $(this).find("textarea").val(storageValue);
-        console.log(newValue);
+        
       });
 
 
